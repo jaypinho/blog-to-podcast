b54de19ab0ec05191cb0ac268e961e1588e472a3
Jay Pinho <jaypinho@gmail.com>
2024-04-28 16:18:39 -0700

    Test POSTs

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
-    return "Hello World!"
+    return request.json
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
